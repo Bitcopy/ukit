2c65ea1be853bbedb6197ecdf66299f24b7a5a0d
nakhoa <nakhoa17@gmail.com>
2021-03-18 00:18:36 +0700

    chore: init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import { UkitSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+   UkitSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <ukit-sample />
+  </div>
+</template>
